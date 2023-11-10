028dbdc28fe18467576ed8d42ed2f057099d5414
Buffalo42 <atustotle@gmail.com>
2023-11-10 11:22:02 +0100

    find_cmd.c

--- find_cmd.c
+++ find_cmd.c
@@ -7,6 +7,25 @@ char *find_cmd(const char *argv, const char *env)
 
 	if ((char *env_cpy = strdup(env)) == NULL)
 		return (NULL);
-	if(strtok(env_cpy) == NULL)
-		
+	char *token = strtok(env_cpy, delim);
+	char output = NULL;
+	while (token != NULL)
+	{
+		if (strcmp(token, "/usr/local/bin" | "/usr/bin" | "/bin") == 0)
+			strcpy(output, token);
+			strcat(output, "/");
+			strcat(output, argv);
+
+		if (access(output, F_OK && X_OK) == 0)
+		{
+			free(env_cpy);
+			return(strdup(output));
+		}
+		else
+			perror("error");
+			exit(-1);
+		token = strtok(NULL, delim);
+	}
+	free(env_cpy);
+	return(NULL);
 }
