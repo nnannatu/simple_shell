b4a63bc2085232a02a6150b4ed7cbb8378990f6d
Buffalo42 <atustotle@gmail.com>
2023-11-12 13:08:42 +0100

    _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -2,20 +2,22 @@
 
 char *_getenv(const char *name)
 {
+	extern char **environ;
 	int i = 0;
-	char **env = NULL;
-	char *env_cpy = NULL;
+	char **env;
+	char *env_cpy;
 	if (name == NULL || environ == NULL)
 		return (NULL);
 
-	for  (*env = environ; *env != NULL; env++)
+	for  (env = environ; *env != NULL; env++)
 	{
 		env_cpy = *env;
 
-		for (i = 0; name[i] != '\0' && env_cpy[i] == name[i]; i++)
-
-		if (name[i] == '\0' && env_cpy[i] == '=')
-			return (env_cpy + i + 1);
+		for (i = 0; strncmp(name, env_cpy, i) == 0; i++)
+		{
+			if (name[i] == '\0' && env_cpy[i] == '=')
+				return (env_cpy + i + 1);
+		}
 	}
 	return (NULL);
 }
