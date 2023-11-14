d79487a0dbe85fa90ef7cb667b4c58de22365f27
Buffalo42 <atustotle@gmail.com>
2023-11-14 16:51:12 +0100

    main.h update

--- main.h
+++ main.h
@@ -11,12 +11,14 @@
 #define MAX_SIZE 1024
 #define MAX_TOKENZ 100
 
+extern char **environ;
 int _putchar(char m);
 void _puts(char *word);
 int _strlen(char *m);
-char *find_cmd(const char *argv, const char *env);
+char *find_cmd(const char *tokenz);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
+char *_getenv(const char *name);
 
 #endif
