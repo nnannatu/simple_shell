6996b9a5c26ec8a99d0884154ebeb88e5de89e3e
Buffalo42 <atustotle@gmail.com>
2023-11-15 12:34:02 +0100

    _strncmp.c

---
+++ _strncmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncmp - compares n number of characters in a string
+ * @str1: the first string.
+ * @str2: the second string.
+ * @n: the number of characters to compare.
+ * Return: the number of compared characters
+ */
+
+int _strncmp(const char *str1, const char *str2, size_t n)
+{
+	for (size_t i = 0; i < n; ++i)
+	{
+		if (str1[i] != str2[i]) 
+		{
+			return ((int)(str1[i] - str2[i]));
+		}
+
+		if (str1[i] == '\0' || str2[i] == '\0')
+		{
+			break;
+		}
+	}
+	return (0);
+}
